aff09c0c6ac3de4856eee231d77c25c59e2442a4
linming.brian <linming.brian@bytedance.com>
2023-09-14 21:27:10 +0800

    p8

--- main.go
+++ main.go
@@ -12,9 +12,11 @@ func main() {
 	go player("ping", table)
 	go player("pong", table)
 
-	// table <- new(Ball) // game on; toss the ball
+	table <- new(Ball) // game on; toss the ball
 	time.Sleep(1 * time.Second)
 	<-table // game over; grab the ball
+
+	panic("show me the stacks")
 }
 
 func player(name string, table chan *Ball) {
