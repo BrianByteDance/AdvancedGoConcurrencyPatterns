3868263d2e88968eb3f1f32e5e47fcf4723b7166
linming.brian <linming.brian@bytedance.com>
2023-09-14 21:32:54 +0800

    p12

--- fetch.go
+++ fetch.go
@@ -2,8 +2,13 @@ package AdvancedGoConcurrencyPatterns
 
 import "time"
 
-// Fetch fetches Items for uri and returns the time when the next
-// fetch should be attempted.  On failure, Fetch returns an error.
-func Fetch(uri string) (items []Item, next time.Time, err error) {
-	return nil, time.Time{}, nil
+type Fetcher interface {
+	// Fetch fetches Items for uri and returns the time when the next
+	// fetch should be attempted.  On failure, Fetch returns an error.
+	Fetch() (items []Item, next time.Time, err error)
+}
+
+// fetches Items from domain
+func Fetch(domain string) Fetcher {
+	return nil
 }
